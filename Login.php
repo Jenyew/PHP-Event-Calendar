2c29e5245b85c9e5ccbf6bbd9c76f7e5a69484e8
Jenyew <robbertpress@outlook.com>
2015-04-17 15:51:39 -0700

    Started Login Function

--- Login.php
+++ Login.php
@@ -10,8 +10,28 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
+        <form action ="login.php" method ="POST" name ="login">
+            <input type ="text" placeholder ="username" name ="username">
+            <input type ="password" placeholder ="password" name ="password">
+            <input type ="submit" value ="login">    
+        </form>
+        <pre>
         <?php
         // put your code here
+       include("lib/Users.php");
+       $loggedIn = logIn($_POST["username"],$_POST["password"]);
+       if ($loggedIn) {
+           print "I am logged in!";
+         
+       } else {
+           print "I am not logged in. D=";
+       }
+        $_POST["face"]["something"] = "Hello";
+       print_r ($_POST);
+       print "My Username is " . $_POST["username"];
+       print "<br />";
+       print "My Password is " . $_POST["face"]["something"];
         ?>
+        </pre>
     </body>
 </html>
--- lib/Users.php
+++ lib/Users.php
@@ -5,4 +5,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+function logIn($username,$pass) {
+    //check if username exists
+    $userExists = true;
+    if ($userExists) {
+            //get user_pass
+        $userPass = "password";
+        if ($userPass == $pass) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
