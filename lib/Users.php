5c7a7e34a54628215990a323808e5818dc1b58a2
Jenyew <robbertpress@outlook.com>
2015-04-20 20:18:48 -0700

    Added base functions to Users.php

--- lib/Users.php
+++ lib/Users.php
@@ -5,6 +5,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+
+
+/**
+ * Logs in the user by checking whether the username exists in the database first
+ * then by checking if the password matches the database password.
+ * @param string $username  The user logging in.
+ * @param string $pass  Their password.
+ * @return array  Success boolean on whether or not login was valid and errorMessage.
+ */
 function logIn($username,$pass) {
     //check if username exists
     $userExists = false;
@@ -19,4 +29,29 @@ function logIn($username,$pass) {
     } else {
         return array("success" => false, "errorMessage" => "Incorrect username!");
     }
+}
+
+function logOut() {
+    
+    session_destroy();
+}
+
+function saveUserData() {
+    
+}
+
+function loadUserData() {
+    
+}
+
+function createUser() {
+    
+}
+
+function deleteUser() {
+    
+}
+
+function loadAllUsers() {
+    
 }
